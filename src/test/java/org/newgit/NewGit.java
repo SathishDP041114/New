110f00ce9c2de428ee5bd8c225138236b8d49e5f
SathishDP041114 <sathishsathish140404@gmail.com>
2022-12-22 15:41:19 -0800

    Code updated

--- src/test/java/org/newgit/NewGit.java
+++ src/test/java/org/newgit/NewGit.java
@@ -3,7 +3,8 @@ package org.newgit;
 public class NewGit {
 
 	public static void main(String[] args) {
-		System.out.println("");
+		System.out.println("Sathish");
+		System.out.println("Sathishkumar");
 		
 	}
 }
